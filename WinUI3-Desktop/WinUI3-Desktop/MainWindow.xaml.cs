8e6d13ac22b67edb7ef3f3c8622bfa01612ff6f4
Monica Chintala <monicach@microsoft.com>
2021-06-25 16:06:10 -0700

    add winui repro

---
+++ WinUI3-Desktop/WinUI3-Desktop/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUI3_Desktop
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            MyWebView.NavigationStarting += Navigation_Starting;
+            MyWebView.NavigationCompleted += Navigation_Completed;
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Uri targetUri = new Uri(addressBar.Text);
+                MyWebView.Source = targetUri;
+                //or
+                //MyWebView.CoreWebView2.Navigate(targetUri.AbsoluteUri);
+            }
+            catch (FormatException ex)
+            {
+            }
+        }
+
+        private void Navigation_Starting(WebView2 sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            String uri = args.Uri;
+        }
+        private void Navigation_Completed(WebView2 sender, CoreWebView2NavigationCompletedEventArgs args)
+        {
+            bool status = args.IsSuccess;
+            var id = args.NavigationId;
+        }
+    }
+}
